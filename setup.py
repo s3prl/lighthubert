aae500d6bc4240498d9397832ab870df7965e16f
Leo Yang <leo19941227@gmail.com>
2022-04-16 20:21:43 +0800

    add submodules

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='lighthubert',
     version='0.1.0',
-    packages=['lighthubert',],
+    packages=['lighthubert', "lighthubert.modules", "lighthubert.functional"],
     license='MIT License'
 )
